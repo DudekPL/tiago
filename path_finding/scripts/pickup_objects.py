29d2ff57106a5f1c31bae91ad59959cb356e13a6
Cezary <>
2021-01-21 21:17:39 +0100

    plan_path service

--- path_finding/scripts/pickup_objects.py
+++ path_finding/scripts/pickup_objects.py
@@ -1,110 +1,23 @@
 #!/usr/bin/env python
 
 import rospy
-import rospkg
-import itertools as itt
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from gazebo_msgs.srv import DeleteModel
 from items_locating.srv import Find_object
-import sqlite3 as sql
 from tf.transformations import quaternion_from_euler
-import random
-from path_finding.srv import pickup_objects_srv, pickup_objects_srvResponse
+from path_finding.srv import pickup_objects_srv, pickup_objects_srvResponse, Plan_path_srv, Plan_path_srvRequest, Plan_path_srvResponse
 
 start_point = 'spawn'
 
 
 class Point:
-
     def __init__(self, _id, _object, _x, _y, _yaw):
         self.id = _id
         self.object = _object
         self.x = _x
         self.y = _y
         self.yaw = _yaw
-
-
-class Object:
-
-    def __init__(self, _name, _expiring, _priority):
-        self.name = _name
-        self.expiring = _expiring
-        self.priority = _priority
-
-
-def get_priority(_point):
-    return objects[_point.object].priority
-
-
-def path_length(_path):
-    prev_p = _path[0]
-    _dist = spawn_dist[_path[0].id]
-    for point in _path:
-        _dist += dist_dict[prev_p.id][point.id]
-        prev_p = point
-    _dist += spawn_dist[prev_p.id]
-    return _dist
-
-
-def plan_path2():
-    sorted_points = sorted(points, reverse=True, key=get_priority)
-    groups = itt.groupby(sorted_points, key=get_priority)
-    whole_path = []
-    for _priority, _group in groups:
-        perm = itt.permutations(_group)
-        shortest_dist = float('inf')
-        shortest_path = []
-        for _path in perm:
-            _dist = path_length(_path)
-            if _dist < shortest_dist:
-                shortest_dist = _dist
-                shortest_path = _path
-        for point in shortest_path:
-            print(point.object + ' ' + str(point.id))
-            whole_path.append(point)
-        whole_path.append(spawn)
-    return whole_path
-
-
-def plan_path(d=2, k=4, Imax=5):
-    shortest_path = []
-    shortest_dist = float('inf')
-    _sorted_points = sorted(points, reverse=True, key=get_priority)
-    for i in range(0, Imax):
-        _path = initialize_GRASP(_sorted_points, d, k)
-        _dist = path_length(_path)
-        if _dist < shortest_dist:
-            shortest_dist = _dist
-            shortest_path = list(_path)
-    return shortest_path
-
-
-def choose_random(_list):
-    return random.choice(_list)
-
-
-def initialize_GRASP(_sorted_points, d, k):
-    _groups = itt.groupby(_sorted_points, key=get_priority)
-    _points_to_visit = list(_sorted_points)
-    _path = []
-    last_point = spawn
-    for priority, group in _groups:
-        d_relaxed = [_p for _p in _points_to_visit if get_priority(_p) >= priority-d]
-        current_group = [_p for _p in d_relaxed if get_priority(_p) == priority]
-        while len(current_group) > 0:  # not empty
-            d_relaxed = sorted(d_relaxed, key=lambda _p: dist_dict[last_point.id][_p.id])
-            restricted_candidates = d_relaxed[:k]
-            print('############################## {} {} {}######################1'.format(len(d_relaxed), len(restricted_candidates), len(current_group)))
-            _random = choose_random(restricted_candidates)
-            _path.append(_random)
-            # remove point from lists
-            if get_priority(_random) == priority:
-                current_group.remove(_random)
-            d_relaxed.remove(_random)
-            _points_to_visit.remove(_random)
-        _path.append(spawn)
-    return _path
 
 
 def goal_msg(_point):
@@ -128,72 +41,15 @@ def goal_msg(_point):
     return _goal
 
 
-def get_objects_from_db(_cur, _objects_names):
-    _query = "SELECT name, expiring, prority FROM objects WHERE name IN ("+",".join(["?"]*len(_objects_names))+")"
-    _cur.execute(_query, _objects_names)
-    _dict = {}
-    for _row in _cur.fetchall():
-        _dict[_row['name']] = Object(_row['name'], _row['expiring'], _row['prority'])
-    return _dict
-
-
-def get_look_up_points_from_db(_cur, _objects):
-    _query = "SELECT * FROM look_up_points WHERE map=(?) AND object IN ("+",".join(["?"]*len(_objects))+")"
-    _args = [world_name]
-    _args.extend([_o.name for _o in _objects])
-    _cur.execute(_query, _args)
-    _list = []
-    for _row in _cur.fetchall():
-        _list.append(Point(_row['id'], _row['object'], _row['x'], _row['y'], _row['yaw']))
-    return _list
-
-
-def get_distances_dict_from_db(_cur, _points, _spawn):
-    _all_points = list(_points)
-    _all_points.append(_spawn)
-    _query = 'SELECT * FROM distances WHERE start IN (' +",".join(["?"] * len(_all_points)) + ")"
-    _query = _query + 'AND finish IN (' +",".join(["?"] * len(_all_points)) + ")"
-    _args = [_p.id for _p in _all_points]
-    _args.extend([_p.id for _p in _all_points])
-    _cur.execute(_query, _args)
-    _dict = {_p.id: {} for _p in _all_points}
-    for _row in _cur.fetchall():
-        _dict[_row['start']][_row['finish']] = _row['distance']
-    return _dict
-
-
-def get_spawn_from_db(_cur, _map):
-    _query = 'SELECT * FROM look_up_points WHERE object=(?) AND map=(?)'
-    _cur.execute(_query, [start_point, _map])
-    _row = _cur.fetchone()
-    return Point(_row['id'], _row['object'], _row['x'], _row['y'], _row['yaw'])
-
-
-def get_spawn_distances_from_db(_cur, _spawn_id):
-    _query = 'SELECT * FROM distances WHERE start=(?)'
-    _cur.execute(_query, [_spawn_id])
-    _dict = {}
-    for _row in _cur.fetchall():
-        _dict[_row['finish']] = _row['distance']
-    return _dict
-
-
 def pickup_object_service(_req):
-    global spawn, objects, points, dist_dict, spawn_dist
     to_pickup = _req.objects
-    # connection to database
-    db_path = rospkg.RosPack().get_path('path_finding')
-    conn = sql.connect(db_path + "/../test.db")
-    conn.row_factory = sql.Row
-    cursor = conn.cursor()
-    # selects from db
-    spawn = get_spawn_from_db(cursor, world_name)
-    objects = get_objects_from_db(cursor, to_pickup)
-    points = get_look_up_points_from_db(cursor, objects.values())
-    dist_dict = get_distances_dict_from_db(cursor, points, spawn)
-    spawn_dist = get_spawn_distances_from_db(cursor, spawn.id)
 
-    path = plan_path()
+    path_request = Plan_path_srvRequest()
+    path_request.world_name = world_name
+    path_request.objects = to_pickup
+
+    path_response = plan_path(path_request)  # type: Plan_path_srvResponse
+    path = path_response.path
 
     move_base_client.wait_for_server()
     picked_up = []
@@ -222,7 +78,6 @@ def pickup_object_service(_req):
             remove_model_srv(p.object)
             picked_up.append(p.object)
             print("############################ picked up ##########################")
-    conn.close()
     response = pickup_objects_srvResponse()
     not_picked_up = list(set(to_pickup)-set(picked_up))
     response.done = len(not_picked_up) == 0
@@ -238,6 +93,9 @@ if __name__ == '__main__':
     # srv for checking if object is seen by robot
     rospy.wait_for_service('find_object_srv')
     find_object = rospy.ServiceProxy('find_object_srv', Find_object)
+    # srv for planning path
+    rospy.wait_for_service('plan_path_srv')
+    plan_path = rospy.ServiceProxy('plan_path_srv', Plan_path_srv)
 
     # loading params
     world_name = rospy.get_param('world_name')
---
+++ path_finding/scripts/plan_path.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python
+
+import rospkg
+import sqlite3 as sql
+import rospy
+from path_finding.srv import Plan_path_srv, Plan_path_srvResponse, Plan_path_srvRequest
+from path_finding.msg import Point
+import itertools as itt
+import random
+
+start_point = 'spawn'
+
+
+class Object:
+    def __init__(self, _name, _expiring, _priority):
+        self.name = _name
+        self.expiring = _expiring
+        self.priority = _priority
+
+
+def get_objects_from_db(_cur, _objects_names):
+    _query = "SELECT name, expiring, prority FROM objects WHERE name IN ("+",".join(["?"]*len(_objects_names))+")"
+    _cur.execute(_query, _objects_names)
+    _dict = {}
+    for _row in _cur.fetchall():
+        _dict[_row['name']] = Object(_row['name'], _row['expiring'], _row['prority'])
+    return _dict
+
+
+def get_look_up_points_from_db(_cur, _objects, _world_name):
+    _query = "SELECT * FROM look_up_points WHERE map=(?) AND object IN ("+",".join(["?"]*len(_objects))+")"
+    _args = [_world_name]
+    _args.extend([_o.name for _o in _objects])
+    _cur.execute(_query, _args)
+    _list = []
+    for _row in _cur.fetchall():
+        _list.append(Point(_row['id'], _row['object'], _row['x'], _row['y'], _row['yaw']))
+    return _list
+
+
+def get_distances_dict_from_db(_cur, _points, _spawn):
+    _all_points = list(_points)
+    _all_points.append(_spawn)
+    _query = 'SELECT * FROM distances WHERE start IN (' + ",".join(["?"] * len(_all_points)) + ")"
+    _query = _query + 'AND finish IN (' + ",".join(["?"] * len(_all_points)) + ")"
+    _args = [_p.id for _p in _all_points]
+    _args.extend([_p.id for _p in _all_points])
+    _cur.execute(_query, _args)
+    _dict = {_p.id: {} for _p in _all_points}
+    for _row in _cur.fetchall():
+        _dict[_row['start']][_row['finish']] = _row['distance']
+    return _dict
+
+
+def get_spawn_from_db(_cur, _map):
+    _query = 'SELECT * FROM look_up_points WHERE object=(?) AND map=(?)'
+    _cur.execute(_query, [start_point, _map])
+    _row = _cur.fetchone()
+    return Point(_row['id'], _row['object'], _row['x'], _row['y'], _row['yaw'])
+
+
+def get_spawn_distances_from_db(_cur, _spawn_id):
+    _query = 'SELECT * FROM distances WHERE start=(?)'
+    _cur.execute(_query, [_spawn_id])
+    _dict = {}
+    for _row in _cur.fetchall():
+        _dict[_row['finish']] = _row['distance']
+    return _dict
+
+
+def get_priority(_point, _objects):
+    return _objects[_point.object].priority
+
+
+def path_length(_path, _spawn_distances, _distance_dictionary):
+    prev_p = _path[0]
+    _dist = _spawn_distances[_path[0].id]
+    for point in _path:
+        _dist += _distance_dictionary[prev_p.id][point.id]
+        prev_p = point
+    _dist += _spawn_distances[prev_p.id]
+    return _dist
+
+
+def choose_random(_list):
+    return random.choice(_list)
+
+
+def initialize_GRASP(_sorted_points, _d, _k, _spawn_point, _distance_dictionary, _objects):
+    _groups = itt.groupby(_sorted_points, key=lambda _p: get_priority(_p, _objects))
+    _points_to_visit = list(_sorted_points)
+    _path = []
+    last_point = _spawn_point
+    for priority, group in _groups:
+        d_relaxed = [_p for _p in _points_to_visit if get_priority(_p, _objects) >= priority - _d]
+        current_group = [_p for _p in d_relaxed if get_priority(_p, _objects) == priority]
+        while len(current_group) > 0:  # not empty
+            d_relaxed = sorted(d_relaxed, key=lambda _p: _distance_dictionary[last_point.id][_p.id])
+            restricted_candidates = d_relaxed[:_k]
+            _random = choose_random(restricted_candidates)
+            _path.append(_random)
+            # remove point from lists
+            if get_priority(_random, _objects) == priority:
+                current_group.remove(_random)
+            d_relaxed.remove(_random)
+            _points_to_visit.remove(_random)
+        _path.append(_spawn_point)
+    return _path
+
+
+# def plan_path2():
+#     sorted_points = sorted(points, reverse=True, key=get_priority)
+#     groups = itt.groupby(sorted_points, key=get_priority)
+#     whole_path = []
+#     for _priority, _group in groups:
+#         perm = itt.permutations(_group)
+#         shortest_dist = float('inf')
+#         shortest_path = []
+#         for _path in perm:
+#             _dist = path_length(_path)
+#             if _dist < shortest_dist:
+#                 shortest_dist = _dist
+#                 shortest_path = _path
+#         for point in shortest_path:
+#             print(point.object + ' ' + str(point.id))
+#             whole_path.append(point)
+#         whole_path.append(spawn)
+#     return whole_path
+
+
+def plan_path(_req, _Imax, _d, _k):
+    """
+
+    :param _k:
+    :param _d:
+    :param _Imax:
+    :type _req: Plan_path_srvRequest
+    """
+    to_pickup = _req.objects
+    world_name = _req.world_name
+    # connection to database
+    db_path = rospkg.RosPack().get_path('path_finding')
+    conn = sql.connect(db_path + "/../test.db")
+    conn.row_factory = sql.Row
+    cursor = conn.cursor()
+    # selects from db
+    spawn = get_spawn_from_db(cursor, world_name)
+    objects = get_objects_from_db(cursor, to_pickup)
+    points = get_look_up_points_from_db(cursor, objects.values(), world_name)
+    dist_dict = get_distances_dict_from_db(cursor, points, spawn)
+    spawn_dist = get_spawn_distances_from_db(cursor, spawn.id)
+
+    shortest_path = []
+    shortest_dist = float('inf')
+    _sorted_points = sorted(points, reverse=True, key=lambda _p: get_priority(_p, objects))
+    for i in range(0, _Imax):
+        _path = initialize_GRASP(_sorted_points, _d, _k, spawn, dist_dict, objects)
+        _dist = path_length(_path, spawn_dist, dist_dict)
+        if _dist < shortest_dist:
+            shortest_dist = _dist
+            shortest_path = list(_path)
+    conn.close()
+    response = Plan_path_srvResponse()
+    response.path = shortest_path
+    return response
+
+
+if __name__ == '__main__':
+    print("############################ service plan path ##########################")
+    rospy.init_node('plan_path')
+
+    Imax = 5
+    d = 2
+    k = 4
+    rospy.Service('plan_path_srv', Plan_path_srv, lambda _req: plan_path(_req, Imax, d, k))
+    rospy.spin()
